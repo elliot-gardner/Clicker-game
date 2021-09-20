97b5c7854572018c7a886198af5288b428375170
elliot-gardner <elliot.g@me.com>
2021-09-20 21:53:47 +0000

    added working timer that counts down and stops at 0, then tells user that the time is up

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 const clickReadout = document.getElementById('clickReadout')
-let totalClicks = "0";
+const timer = document.getElementById('timer')
 
+let totalClicks = "0";
+let timeAllowed = 10;
+
+// This is the click counter  & the event listener that tracks the clicks //
 function clickCounter() {
     console.log("Click")
     totalClicks ++;
@@ -8,3 +12,24 @@ function clickCounter() {
 }
 
 document.addEventListener('click', clickCounter);
+
+// Function to let the user know that the game is over//
+function endGame() {
+    alert("Times up!");
+}
+
+// Countdown function, if there is remaining time then time allowed is decremented by 1, stops counting down at 0 then calls the endgame function //
+function countDown() {
+    if (timeAllowed <= 0) {
+    clearInterval(countDownTimer);
+    endGame();
+} 
+else {
+    console.log("Countdown function works");
+    timeAllowed -= 1;
+    timer.innerHTML = "Time: " + timeAllowed;
+}
+}
+
+// this variable calls the function "countDown" every 1000ms (1 second) //
+var countDownTimer = setInterval(countDown, 1000);
