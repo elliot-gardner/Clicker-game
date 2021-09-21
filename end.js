db78acafce20b3ac43fa343648d3651931a6ab82
elliot-gardner <elliot.g@me.com>
2021-09-21 07:28:37 +0000

    added code to save user name and score to local storage

--- end.js
+++ end.js
@@ -2,6 +2,23 @@ const latestScore = localStorage.getItem("latestScore");
 const recentScore = document.getElementById("recentScore");
 const username = document.getElementById("name")
 
+// Code below is from James Q Quick's youtube tutorial on saving high scores to local storage. //
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+saveScore = e => {
+e.preventDefault();
+
+const score = {
+    score: latestScore,
+    name: username.value
+};
+highScores.push(score);
+highScores.sort((a, b) => b.score - a.score);
+highScores.splice(5);
+
+localStorage.setItem('highScores', JSON.stringify(highScores));
+window.location.replace("/highscores.html");
+}
 
 //Gives score readout//
 recentScore.innerHTML = "You Scored: " + latestScore + "!"
