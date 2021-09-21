3ff327586292d56a8efda0339ec9317e495a5bd6
elliot-gardner <elliot.g@me.com>
2021-09-21 07:38:22 +0000

    highscores.js now pulls high scores from local storage & updates highscores.html

---
+++ highscores.js
@@ -0,0 +1,8 @@
+const scoresList = document.getElementById('scores');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+//This pulls the name & score from local storage and adds them to the unordered list element in the html//
+scoresList.innerHTML =
+highScores.map(score => {
+    return `<li class="scores">${score.name} - ${score.score}</li>`
+}).join("")
